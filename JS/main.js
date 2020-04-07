e7219de6a67f96e7dcca22a6dfaa1abb19cbae20
HamiltonMassango <hamiltonsilva551@gmail.com>
2020-04-07 22:47:08 +0100

    Falta

--- JS/main.js
+++ JS/main.js
@@ -7,6 +7,7 @@
         },
         dados: '',
     }
+
     await axios({
         "method": "GET",
         "url": "https://covid-193.p.rapidapi.com/statistics",
@@ -19,62 +20,58 @@
         return response.data;
     }).then((date) => {
         state.dados = date;
-        document.querySelector("#valor").innerHTML = JSON.stringify(date) ;
+        document.querySelector("#valor").innerHTML = JSON.stringify(date);
     }).catch((error) => {
         console.log(error)
     });
     document.querySelector('button.button').addEventListener('click', loadCountry);
 
     function show(e) {
-        document.querySelector('[data-js="result"]').innerHTML = `
-    <div>
-    <h1> ${e.country} </h1>
-</div>
-<span>Data ${e.time.slice(0,10)}</span>
-<div id="estatistica">
-    <div>
-        <h3>Casos Novos</h3>
-        <span>${e.cases.new}</span>
-    </div>
-    <div>
-        <h3>Total</h3>
-        <span>${e.cases.total}</span>
-    </div>
-    <div>
-        <h3>Ativo</h3>
-        <span>${e.cases.active}</span>
-    </div>
-    <div>
-        <h3>Crítico</h3>
-        <span>${e.cases.critical}</span>
-    </div>
-    <div>
-        <h3>Recuperado</h3>
-        <span>${e.cases.recovered}</span>
-    </div>
-    
-    <div>
-        <h3>Total de Mortes</h3>
-        <span>${e.deaths.total}</span>
-    </div>
-</div>
-    `;
+        return document.querySelector('[data-js="result"]').innerHTML = `
+        <div>
+        <h1> ${e.country} </h1>
+        </div>
+        <span>Data ${e.time.slice(0,10)}</span>
+        <div id="estatistica">
+            <div>
+                <h3>Casos Novos</h3>
+                <span>${e.cases.new}</span>
+            </div>
+            <div>
+                <h3>Total</h3>
+                <span>${e.cases.total}</span>
+            </div>
+            <div>
+                <h3>Ativo</h3>
+                <span>${e.cases.active}</span>
+            </div>
+            <div>
+                <h3>Crítico</h3>
+                <span>${e.cases.critical}</span>
+            </div>
+            <div>
+                <h3>Recuperado</h3>
+                <span>${e.cases.recovered}</span>
+            </div>
+            
+            <div>
+                <h3>Total de Mortes</h3>
+                <span>${e.deaths.total}</span>
+            </div>
+        </div>
+        `;
     }
-
-
-
 
     function loadCountry() {
         const input = document.querySelector('input').value;
         // img in loading 
 
         document.querySelector('[data-js="result"]').innerHTML = `
-        <img
-        src="./image/loading.gif"
-        alt="Covid-19"
-        class="loading"
-    />
-    `;
+            <img
+                src="./image/loading.gif"
+                alt="Covid-19"
+                class="loading"
+            />`;
         state.dados.response.forEach((e, id) => {
             if (e.country.includes(input.slice(0, 1).toLocaleUpperCase() + input.slice(1))) {
                 show(e);
@@ -91,45 +88,62 @@
     `;
         }
     }
+
 })();
+
+/* Auto complete */
 (async function () {
     const valorCountry = {
-        stateGet : [],
-        stateSet : function (valor){
+        stateGet: [],
+        stateSet: function (valor) {
             return this.stateGet = valor;
         },
-        autoComplete : function (country){
-            const countryComplete =  this.stateGet;
+        autoComplete: function (country) {
+            const countryComplete = this.stateGet;
             return countryComplete.filter((value) => {
                 return value.country.includes(country);
             })
         }
     };
-
-    await fetch('./JS/req.json')
-            .then((dados)=> dados.json())
-            .then((dados)=> {
-                return valorCountry.stateSet(dados.response);
+    function addEvente(){      
+        document.querySelectorAll('.sugg').forEach((a)=>{
+            a.addEventListener('click', ()=>{
+                document.querySelector('input').value = a.childNodes[0].wholeText;
+                loadCountry();
             })
-            .catch((error)=> {
-                console.log(error)
-            });
+        })
+    }
+    await fetch('./JS/req.json')
+        .then((dados) => dados.json())
+        .then((dados) => {
+            return valorCountry.stateSet(dados.response);
+        })
+        .catch((error) => {
+            console.log(error)
+        });
 
-        document.querySelector('#input').addEventListener("input", autocompleteDisplay)
+    document.querySelector('#input').addEventListener("input", autocompleteDisplay)
 
-    function autocompleteDisplay({target}){
+    function autocompleteDisplay({ target }) {
+        const Sugestao = document.querySelector('#sugestao');
         const dataCamp = target.value;
-        if(dataCamp.length){
+        if (dataCamp.length) {
             const valueComplet = valorCountry.autoComplete(dataCamp);
-            document.querySelector('#sugestao').innerHTML = ``;
+            Sugestao.innerHTML = ``;
             for (let index = 0; index < 3; index++) {
-                document.querySelector('#sugestao').innerHTML += `
-                    <p>${valueComplet[index].country}</p>
+                if(valueComplet[index]){
+                    Sugestao.innerHTML += `
+                    <p class="sugg">${valueComplet[index].country}</p>
                     `;
-        }          
-        }else {
-            document.querySelector('#sugestao').innerHTML = ``;
+                }
+                
+            }
+            if(!!valueComplet[0]){
+                addEvente();
+            }
+        } else {
+            Sugestao.innerHTML = ``;
         }
-        
+
     }
 })();
